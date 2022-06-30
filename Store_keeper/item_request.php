b511d15e9e5a5c6fa92ec9c3589dcb01ce4066ef
annmeth <annethnissar@gmail.com>
2022-06-30 07:50:15 +0300

    small changes

--- Store_keeper/item_request.php
+++ Store_keeper/item_request.php
@@ -20,94 +20,91 @@ $rrsn = "";
 // connect to the database
 //$db = mysqli_connect('localhost', 'root', '', 'medicare');
 //if (mysqli_connect_errno())
-  //  {
-  //  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  //  }
+//  {
+//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//  }
 
 // Add item
 if (isset($_POST['add_data'])) {
-  // receive all input values from the form
-  //echo "connect";
-  $strid = mysqli_real_escape_string($db, $_POST['strid']);
-  $skname  = mysqli_real_escape_string($db, $_POST['skname']);
-  $rittype      = mysqli_real_escape_string($db, $_POST['rittype']);
-  $ritname   = mysqli_real_escape_string($db, $_POST['ritname']);
-  $qty     = mysqli_real_escape_string($db, $_POST['qty']);
-  $rrsn     = mysqli_real_escape_string($db, $_POST['rrsn']);
+   // receive all input values from the form
+   //echo "connect";
+   $strid = mysqli_real_escape_string($db, $_POST['strid']);
+   $skname  = mysqli_real_escape_string($db, $_POST['skname']);
+   $rittype      = mysqli_real_escape_string($db, $_POST['rittype']);
+   $ritname   = mysqli_real_escape_string($db, $_POST['ritname']);
+   $qty     = mysqli_real_escape_string($db, $_POST['qty']);
+   $rrsn     = mysqli_real_escape_string($db, $_POST['rrsn']);
 
 
 
-    $query = "INSERT INTO request (strid,skname,rittype,ritname,qty,rrsn)
+   $query = "INSERT INTO request (strid,skname,rittype,ritname,qty,rrsn)
   			  VALUES('$strid','$skname','$rittype','$ritname','$qty','$rrsn')";
-      if(mysqli_query($db, $query))
-      {
+   if (mysqli_query($db, $query)) {
       echo "<script>alert('Successfully stored');</script>";
+   } else {
+      echo "<script>alert('Somthing wrong!!!');</script>";
+   }
 
-    }
-    else{
-        echo"<script>alert('Somthing wrong!!!');</script>";
-    }
-
-  //	header('location: hospital.php');
+   //	header('location: hospital.php');
 
 }
 ?>
 
 
- <div class="main-panel">
-         <div class="content-wrapper">
-           <div class="page-header">
-             <h3 class="page-title"> Items Requests </h3>
-           </div>
-           <form action="item_request.php" method="POST">
-           <div class="body">
+<div class="main-panel">
+   <div class="content-wrapper">
+      <div class="page-header">
+         <h3 class="page-title"> Items Requests </h3>
+      </div>
+      <form action="item_request.php" method="POST">
+         <div class="body">
 
-              <!-- <div class="form-group">
+            <!-- <div class="form-group">
                   <label for="">Installation Date </label>
                   <input type="number" id="inst_date" class="form-control" placeholder="">
                </div>-->
 
-               <div class="form-group">
-                  <label for=""> Store ID </label>
-                  <input type="varchar" name="strid" id="strid" class="form-control" placeholder="" required>
-                </div>
-               <div class="form-group">
-                  <label for=""> Store Keeper's Name </label>
-                  <input type="varchar" name="skname" id="skname" class="form-control" placeholder="" required>
-               </div>
-               <div class="form-group">
-                  <label for=""> Requested Item Type </label>
-                  <input type="varchar" name="rittype" id="rittype" class="form-control" placeholder="" required>
-               </div>
-               <div class="form-group">
-                  <label for=""> Requested Item Name </label>
-                  <input type="varchar" name="ritname" id="ritname" class="form-control" placeholder="" required>
-               </div>
-               <div class="form-group">
-                  <label for=""> Quantity </label>
-                  <input type="varchar" name="qty" id="qty" class="form-control" placeholder="" required>
-               </div>
+            <div class="form-group">
+               <label for=""> Store ID </label>
+               <input type="varchar" name="strid" id="strid" class="form-control" placeholder="" required>
+            </div>
+            <div class="form-group">
+               <label for=""> Store Keeper's Name </label>
+               <input type="varchar" name="skname" id="skname" class="form-control" placeholder="" required>
+            </div>
+            <div class="form-group">
+               <label for=""> Requested Item Type </label>
+               <input type="varchar" name="rittype" id="rittype" class="form-control" placeholder="Supply or Equipment" required>
+            </div>
+            <div class="form-group">
+               <label for=""> Requested Item Name </label>
+               <input type="varchar" name="ritname" id="ritname" class="form-control" placeholder="" required>
+            </div>
+            <div class="form-group">
+               <label for=""> Quantity </label>
+               <input type="varchar" name="qty" id="qty" class="form-control" placeholder="" required>
+            </div>
 
-               <div class="form-group">
-                  <label for=""> Reason for Requesting </label>
-                  <input type="varchar" name="rrsn" id="rrsn" class="form-control" placeholder="" required>
-               </div>
-
-
-              <button type="submit" class="btn btn-primary" name="add_data">Save</button>
+            <div class="form-group">
+               <label for=""> Reason for Requesting </label>
+               <input type="varchar" name="rrsn" id="rrsn" class="form-control" placeholder="" required>
+            </div>
 
 
-             </form>
+            <button type="submit" class="btn btn-primary" name="add_data">Save</button>
 
 
+      </form>
 
 
 
 
-           </div>
+
 
    </div>
-   <?php
-   include 'includes/footer.php';
-   include 'includes/scripts.php';
-   ?>
+
+</div>
+<?php
+include 'includes/footer.php';
+include 'includes/scripts.php';
+?>
--- nurse/add_request.php
+++ nurse/add_request.php
@@ -19,9 +19,9 @@ $rrsn = "";
 // connect to the database
 //$db = mysqli_connect('localhost', 'root', '', 'medicare');
 //if (mysqli_connect_errno())
-  //  {
-  //  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  //  }
+//  {
+//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//  }
 
 // Add item
 if (isset($_POST['add_data'])) {
@@ -35,16 +35,13 @@ if (isset($_POST['add_data'])) {
 
 
 
-    $query = "INSERT INTO dep_request (nsname,rittype,ritname,qty,rrsn)
+  $query = "INSERT INTO dep_request (nsname,rittype,ritname,qty,rrsn)
   			  VALUES('$nsname','$rittype','$ritname','$qty','$rrsn')";
-      if(mysqli_query($db, $query))
-      {
-      echo "<script>alert('Successfully stored');</script>";
-
-    }
-    else{
-        echo"<script>alert('Somthing wrong!!!');</script>";
-    }
+  if (mysqli_query($db, $query)) {
+    echo "<script>alert('Successfully stored');</script>";
+  } else {
+    echo "<script>alert('Somthing wrong!!!');</script>";
+  }
 
   //	header('location: hospital.php');
 
@@ -52,52 +49,52 @@ if (isset($_POST['add_data'])) {
 ?>
 
 
- <div class="main-panel">
-         <div class="content-wrapper">
-           <div class="page-header">
-             <h3 class="page-title"> Make A New Request </h3>
-           </div>
-           <form action="add_request.php" method="POST">
-           <div class="body">
+<div class="main-panel">
+  <div class="content-wrapper">
+    <div class="page-header">
+      <h3 class="page-title"> Make A New Request </h3>
+    </div>
+    <form action="add_request.php" method="POST">
+      <div class="body">
 
 
-               <div class="form-group">
-                  <label for=""> Nurse In Charge's Name </label>
-                  <input type="varchar" name="nsname" id="nsname" class="form-control" placeholder="" required>
-               </div>
-               <div class="form-group">
-                  <label for=""> Requested Item Type </label>
-                  <input type="varchar" name="rittype" id="rittype" class="form-control" placeholder="Supply or Equipment" required>
-               </div>
-               <div class="form-group">
-                  <label for=""> Requested Item Name </label>
-                  <input type="varchar" name="ritname" id="ritname" class="form-control" placeholder="" required>
-               </div>
-               <div class="form-group">
-                  <label for=""> Quantity </label>
-                  <input type="varchar" name="qty" id="qty" class="form-control" placeholder="" required>
-               </div>
+        <div class="form-group">
+          <label for=""> Nurse In Charge's Name </label>
+          <input type="varchar" name="nsname" id="nsname" class="form-control" placeholder="" required>
+        </div>
+        <div class="form-group">
+          <label for=""> Requested Item Type </label>
+          <input type="varchar" name="rittype" id="rittype" class="form-control" placeholder="Supply or Equipment" required>
+        </div>
+        <div class="form-group">
+          <label for=""> Requested Item Name </label>
+          <input type="varchar" name="ritname" id="ritname" class="form-control" placeholder="" required>
+        </div>
+        <div class="form-group">
+          <label for=""> Quantity </label>
+          <input type="varchar" name="qty" id="qty" class="form-control" placeholder="" required>
+        </div>
 
-               <div class="form-group">
-                  <label for=""> Reason for Requesting </label>
-                  <input type="varchar" name="rrsn" id="rrsn" class="form-control" placeholder="" required>
-               </div>
-
-
-              <button type="submit" class="btn btn-primary" name="add_data">Save</button>
+        <div class="form-group">
+          <label for=""> Reason for Requesting </label>
+          <input type="varchar" name="rrsn" id="rrsn" class="form-control" placeholder="" required>
+        </div>
 
 
-             </form>
+        <button type="submit" class="btn btn-primary" name="add_data">Save</button>
 
 
-
+    </form>
 
 
 
-           </div>
 
-   </div>
-   <?php
-   include 'includes/footer.php';
-   include 'includes/scripts.php';
-   ?>
+
+
+  </div>
+
+</div>
+<?php
+include 'includes/footer.php';
+include 'includes/scripts.php';
+?>
